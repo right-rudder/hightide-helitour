fcf59e636ddf96c29abd1f5dd706c6776ed12b00
Raul Ospina <raminka13@hotmail.com>
2024-04-14 16:18:54 -0500

    Update: navbar and footer links

--- src/navigation.js
+++ src/navigation.js
@@ -1,183 +1,151 @@
-import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+import { getPermalink, getBlogPermalink, getAsset } from "./utils/permalinks";
 
 export const headerData = {
   links: [
     {
-      text: 'Homes',
+      text: "Locations",
       links: [
         {
-          text: 'SaaS',
-          href: getPermalink('/homes/saas'),
+          text: "Southport",
+          href: getPermalink("/locations/southport"),
         },
         {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
+          text: "Beaufort",
+          href: getPermalink("/locations/beaufort"),
         },
         {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
-        },
-        {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
+          text: "St. Simons Island",
+          href: getPermalink("/locations/st-simons-island"),
         },
       ],
     },
     {
-      text: 'Pages',
+      text: "The High Tide Experience",
+      href: getPermalink("/the-experience"),
+    },
+    {
+      text: "Other Packages",
       links: [
         {
-          text: 'Features (Anchor Link)',
-          href: getPermalink('/#features'),
+          text: "Book a Custom Tour!",
+          href: getPermalink("/packages/custom-tour"),
         },
         {
-          text: 'Services',
-          href: getPermalink('/services'),
+          text: "Aerial Photography",
+          href: getPermalink("/packages/aerial-photography"),
         },
         {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
-        },
-        {
-          text: 'About us',
-          href: getPermalink('/about'),
-        },
-        {
-          text: 'Contact',
-          href: getPermalink('/contact'),
-        },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
+          text: "Aerial Videography",
+          href: getPermalink("/packages/aerial-videography"),
         },
       ],
     },
     {
-      text: 'Landing',
-      links: [
-        {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
-        },
-        {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
-        },
-        {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
-        },
-      ],
+      text: "Gift Cards",
+      href: getPermalink("/gift-cards"),
     },
     {
-      text: 'Blog',
+      text: "About Us",
       links: [
         {
-          text: 'Blog List',
-          href: getBlogPermalink(),
+          text: "Why High Tide?",
+          href: getPermalink("/about/why-high-tide"),
         },
         {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
+          text: "Testimonials",
+          href: getPermalink("/about/testimonials"),
         },
       ],
-    },
-    {
-      text: 'Widgets',
-      href: '#',
     },
   ],
-  actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
+  actions: [
+    {
+      text: "Book a Tour",
+      href: "#locations",
+      target: "_self",
+    },
+  ],
 };
 
 export const footerData = {
   links: [
     {
-      title: 'Product',
+      title: "Locations",
       links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
+        {
+          text: "Southport",
+          href: getPermalink("/locations/southport"),
+        },
+        {
+          text: "Beaufort",
+          href: getPermalink("/locations/beaufort"),
+        },
+        {
+          text: "St. Simons Island",
+          href: getPermalink("/locations/st-simons-island"),
+        },
       ],
     },
     {
-      title: 'Platform',
+      title: "Packages",
       links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
+        {
+          text: "Book a Custom Tour!",
+          href: getPermalink("/packages/custom-tour"),
+        },
+        {
+          text: "Aerial Photography",
+          href: getPermalink("/packages/aerial-photography"),
+        },
+        {
+          text: "Aerial Videography",
+          href: getPermalink("/packages/aerial-videography"),
+        },
       ],
     },
     {
-      title: 'Support',
+      title: "About Us",
       links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
-      ],
-    },
-    {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
+        {
+          text: "Why High Tide?",
+          href: getPermalink("/about/why-high-tide"),
+        },
+        {
+          text: "Testimonials",
+          href: getPermalink("/about/testimonials"),
+        },
       ],
     },
   ],
   secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
+    { text: "Terms", href: getPermalink("/terms") },
+    { text: "Privacy Policy", href: getPermalink("/privacy") },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    {
+      ariaLabel: "YouTube",
+      icon: "tabler:brand-youtube",
+      href: "https://www.youtube.com/@hightideaviation",
+    },
+    {
+      ariaLabel: "Instagram",
+      icon: "tabler:brand-instagram",
+      href: "https://www.instagram.com/hightideaviation/",
+    },
+    {
+      ariaLabel: "Facebook",
+      icon: "tabler:brand-facebook",
+      href: "https://facebook.com/High-Tide-Aviation-763218714053314/",
+    },
+    { ariaLabel: "RSS", icon: "tabler:rss", href: getAsset("/rss.xml") },
+    /* {
+      ariaLabel: "Github",
+      icon: "tabler:brand-github",
+      href: "https://github.com/onwidget/astrowind",
+    }, */
   ],
   footNote: `
-    <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(src/assets/favicons/rrm-favicon.png)]"></span>
+    Made by <a class="text-blue-600 underline dark:text-muted" href="https://rightruddermarketing.com/"> Right Rudder Marketing</a> · All rights reserved.
   `,
 };
