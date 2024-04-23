0e063699b246217f8a3f8f7bca339b35d11f236c
Raul Ospina <raminka13@hotmail.com>
2024-04-22 21:06:01 -0500

    Update: Add "custom" variant to CallToAction type

--- src/types.d.ts
+++ src/types.d.ts
@@ -180,7 +180,7 @@ export interface Disclaimer {
 
 // COMPONENTS
 export interface CallToAction extends Omit<HTMLAttributes<"a">, "slot"> {
-  variant?: "primary" | "secondary" | "tertiary" | "link";
+  variant?: "primary" | "secondary" | "tertiary" | "link" | "custom";
   text?: string;
   icon?: string;
   classes?: Record<string, string>;
