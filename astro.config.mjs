965fafe304854709e5a86cddc4a95f4478408e69
Diego <diegoyon@outlook.com>
2024-05-22 10:04:16 -0600

    add redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,57 +1,108 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { defineConfig, squooshImageService } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
-import partytown from '@astrojs/partytown';
-import icon from 'astro-icon';
-import compress from 'astro-compress';
-import astrowind from './src/integration';
-import { readingTimeRemarkPlugin, responsiveTablesRehypePlugin, lazyImagesRehypePlugin } from './src/utils/frontmatter.mjs';
+import path from "path";
+import { fileURLToPath } from "url";
+import { defineConfig, squooshImageService } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
+import icon from "astro-icon";
+import compress from "astro-compress";
+import astrowind from "./src/integration";
+import {
+  readingTimeRemarkPlugin,
+  responsiveTablesRehypePlugin,
+  lazyImagesRehypePlugin,
+} from "./src/utils/frontmatter.mjs";
 import react from "@astrojs/react";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const hasExternalScripts = false;
-const whenExternalScripts = (items = []) => hasExternalScripts ? Array.isArray(items) ? items.map(item => item()) : [items()] : [];
+const whenExternalScripts = (items = []) =>
+  hasExternalScripts
+    ? Array.isArray(items)
+      ? items.map((item) => item())
+      : [items()]
+    : [];
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), sitemap(), mdx(), icon({
-    include: {
-      tabler: ['*'],
-      'flat-color-icons': ['template', 'gallery', 'approval', 'document', 'advertising', 'currency-exchange', 'voice-presentation', 'business-contact', 'database']
-    }
-  }), ...whenExternalScripts(() => partytown({
-    config: {
-      forward: ['dataLayer.push']
-    }
-  })), compress({
-    CSS: true,
-    HTML: {
-      'html-minifier-terser': {
-        removeAttributeQuotes: false
-      }
-    },
-    Image: false,
-    JavaScript: true,
-    SVG: false,
-    Logger: 1
-  }), astrowind(), react()],
+  output: "static",
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+    mdx(),
+    icon({
+      include: {
+        tabler: ["*"],
+        "flat-color-icons": [
+          "template",
+          "gallery",
+          "approval",
+          "document",
+          "advertising",
+          "currency-exchange",
+          "voice-presentation",
+          "business-contact",
+          "database",
+        ],
+      },
+    }),
+    ...whenExternalScripts(() =>
+      partytown({
+        config: {
+          forward: ["dataLayer.push"],
+        },
+      })
+    ),
+    compress({
+      CSS: true,
+      HTML: {
+        "html-minifier-terser": {
+          removeAttributeQuotes: false,
+        },
+      },
+      Image: false,
+      JavaScript: true,
+      SVG: false,
+      Logger: 1,
+    }),
+    astrowind(),
+    react(),
+  ],
   image: {
-    service: squooshImageService()
+    service: squooshImageService(),
   },
   markdown: {
     remarkPlugins: [readingTimeRemarkPlugin],
-    rehypePlugins: [responsiveTablesRehypePlugin, lazyImagesRehypePlugin]
+    rehypePlugins: [responsiveTablesRehypePlugin, lazyImagesRehypePlugin],
   },
   vite: {
     resolve: {
       alias: {
-        '~': path.resolve(__dirname, './src')
-      }
-    }
-  }
+        "~": path.resolve(__dirname, "./src"),
+      },
+    },
+  },
+  redirects: {
+    "/fly-with-us-st-simons-island-ga/": "/st-simons-island",
+    "/flight-training-southport-nc/": "/southport",
+    "/fly-with-us-wilmington-nc/": "/southport",
+    "/flight-training-st-simons-island-ga/": "https://hightideaviation.com/",
+    "/specials/": "/southport",
+    "/flight-school-wilmington-nc/": "https://hightideaviation.com/",
+    "/flight-school-southport-nc/flight-lessons/":
+      "https://hightideaviation.com/",
+    "/flight-school-st-simons-island-ga/": "https://hightideaviation.com/",
+    "/aircraft-services-southport-nc/": "https://hightideaviation.com/",
+    "/flight-training-southport-nc/airplane/": "https://hightideaviation.com/",
+    "/fly-with-us-southport-nc/scenic-air-tours/": "/southport",
+    "/fly-with-us-southport-nc/helicopter-flights/": "/southport",
+    "/fly-with-us-st-simons-island-ga/scenic-air-tours/": "/st-simons-island",
+    "/flight-school-southport-nc/": "https://hightideaviation.com/",
+    "/category/flight-training-aerial-services-blog/":
+      "https://hightideaviation.com/blog/",
+    "/contact-high-tide-aviation/": "/contact",
+    "/financing/": "https://hightideaviation.com/about/financing/",
+  },
 });
