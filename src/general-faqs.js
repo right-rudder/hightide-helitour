677dc4788de647f52b2dd18d0b4108ef80b0a0de
Raul Ospina <raminka13@hotmail.com>
2024-05-30 17:28:04 -0500

    New: FAQs components and const

---
+++ src/general-faqs.js
@@ -0,0 +1,28 @@
+import List from "./components/blog/List.astro";
+
+export const GeneralFAQs = [
+  {
+    title: "Where can I take a helicopter tour?",
+    content: `Helicopter tours are available in Southport. It's a beautiful location offering stunning views and an unforgettable experience.`,
+  },
+  {
+    title: "How many passengers are needed for a tour?",
+    content: `The number of passengers required for a tour depends on the type of aircraft:`,
+    list: [
+      "Helicopter tours require 2 to 3 passengers.",
+      "Cessna tours can accommodate between 1 and 3 passengers.",
+    ],
+  },
+  {
+    title: "Is there a weight limit for the tour?",
+    content: `Yes, the combined weight limit for all passengers is 550 pounds. This ensures a safe and comfortable experience for everyone on board.`,
+  },
+  {
+    title: "What is the minimum age requirement for a tour?",
+    content: `Children aged 3 years and older are required to have their own ticket for the tour.`,
+  },
+  {
+    title: "Are headsets included in the tour?",
+    content: `Yes, headsets are included in the tour. They allow you to communicate with the pilot and other passengers, ensuring you don't miss any of the commentary or exciting details during the tour.`,
+  },
+];
