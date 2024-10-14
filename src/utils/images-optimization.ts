ead1d8fc4da844af8df70ccde31fff2eeae107a5
Diego <diegoyon@outlook.com>
2024-10-14 16:09:32 -0600

    remove image optimization sizes

--- src/utils/images-optimization.ts
+++ src/utils/images-optimization.ts
@@ -41,25 +41,9 @@ export type ImagesOptimizer = (
 /* ******* */
 const config = {
   // FIXME: Use this when image.width is minor than deviceSizes
-  imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  imageSizes: [],
 
-  deviceSizes: [
-    640, // older and lower-end phones
-    750, // iPhone 6-8
-    828, // iPhone XR/11
-    960, // older horizontal phones
-    1080, // iPhone 6-8 Plus
-    1280, // 720p
-    1668, // Various iPads
-    1920, // 1080p
-    2048, // QXGA
-    2560, // WQXGA
-    3200, // QHD+
-    3840, // 4K
-    4480, // 4.5K
-    5120, // 5K
-    6016, // 6K
-  ],
+  deviceSizes: [],
 
   formats: ["image/webp"],
 };
